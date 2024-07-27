b2d602acec9b047e13567984bccaea0be556df58
SanixWheee <122181091+SanixWheee@users.noreply.github.com>
2024-07-27 15:45:05 -0700

    commitging!

--- summeronline/src/App.js
+++ summeronline/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className = '' >
-        <p>Website Name</p>
+    <div className = 'flex justify-center border-red-800 border-8' >
+        <p className = ''>Website Name</p>
     </div>
   );
 }
--- summeronline/tailwind.config.js
+++ summeronline/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/*/.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
 }
 
