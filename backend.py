68a7e9d09b0e54f4b4417ed66f9c8e165b8c991f
datboi212 <datboi212mc@gmail.com>
2024-07-27 15:52:49 -0700

    update backend

--- backend.py
+++ backend.py
@@ -7,14 +7,30 @@ import json
 app = Flask(__name__)
 CORS(app)
 load_dotenv()
-url='localhost:5000'
-dataPath = "logInData.json"
+url = "localhost:5000"
 
+userDataPath = 'userData.json'
+
+def get_user_data():
+    with open(userDataPath, 'r') as f:
+        return json.load(f)
+    
+@app.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    with open(dataPath, 'r') as file:
-        logInData = json.load(file)
-    if data['username'] in logInData and data['password'] == logInData[data['username']]:
-        return {"message": "success"}
+    data=request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'uname and password are req'}), 400
+    
+    userData = get_user_data()
+
+    if username in userData and userData[username]['password'] == password:
+        return jsonify({"message": "Logged in"}), 200
     else:
-        return {"message": "failure"}
+        return jsonify({"error": "wrong username or password"}), 401
+
+
+if __name__ == '__main__':
+    app.run()
