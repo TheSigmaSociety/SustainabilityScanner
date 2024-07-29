e4a16dd3a96edc52ccfe212fc516415d3360e6a6
gamedoescool <120051013+gamedoescool@users.noreply.github.com>
2024-07-28 20:16:21 -0700

    (har har har har har)

--- backend/utils.py
+++ backend/utils.py
@@ -7,8 +7,7 @@ def encodeImage(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-def openaiRequest(imagePath, prompt):
-    base64Image = encodeImage(imagePath)
+def openaiRequest(image, prompt):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {os.getenv('KEY')}",
@@ -22,7 +21,7 @@ def openaiRequest(imagePath, prompt):
                     {"type": "text", "text": prompt},
                     {
                         "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64Image}"},
+                        "image_url": {"url": f"data:image/jpeg;base64,{image}"},
                     },
                 ],
             }
