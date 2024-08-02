88167b7e4c7c5be2b0ba0626bafb1df4d393f7da
gamedoescool <120051013+gamedoescool@users.noreply.github.com>
2024-08-01 23:09:24 -0700

    Chat har har har har har

--- backend/backend.py
+++ backend/backend.py
@@ -31,7 +31,7 @@ def login(): #{username:<>,password:<>}
     # else:
     #     return jsonify({"error": "wrong username or password"}), 401
 
-
+#cap HAR HAR HAR HAR HAR HAR AHR HAR HAR HAR HAR HAR
 @app.route("/upload", methods=["POST"])
 def getImage():  # {image:<actualImage>}
     base64_string = request.get_json()["image"]
@@ -44,11 +44,25 @@ def getImage():  # {image:<actualImage>}
         utils.storeImage(output["name"])
     return jsonify(output), 200
 
-@app.route("/getItem",methods=['GET'])
-def getItem():
-    value = request.args.get('place')
-    
+@app.route("/getItem/<place>",methods=['GET'])
+#place - the first item
+#place = 0 -> 0th to 10th item
+#place = 1 -> 11th item to 20th item
+#place = 2 -> 21st to 30th item etc...
+def getItem(place):
+    #do sql majik to get top things
+    return jsonify({"1":{"name":"freddy fazbear","score":69,"description":["har","har","har"]}})
 
+@app.route("/getImage/<name>",methods=['GET'])
+def getImage(name):
+    name += ".txt"
+    files = os.listdir(r"backend")
+    for f in files:
+        if(name == f):
+            with open(os.path.join(r"images",name),"r") as file:
+                return jsonify({"image":file.read()}), 200
+    return 400
 if __name__ == "__main__":
     utils.sqlInit()
     app.run("127.0.0.1", 5001)
+
